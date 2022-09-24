c22f0b5a489931de4495dd52e4eaa7e6f8e6990c
Tom <72009310+HPCTom@users.noreply.github.com>
2022-09-24 12:48:13 +0200

    Update Installation.sh

--- Images_ascii_Cuda_static_sz_COLOR/Installation.sh
+++ Images_ascii_Cuda_static_sz_COLOR/Installation.sh
@@ -1,9 +1,5 @@
 sudo apt install python3-pip
 sudo apt-get install libfreeimage3 libfreeimage-dev
-sudo apt-get install imagemagick
-sudo chmod 777 /etc/ImageMagick-6/policy.xml
-sudo rm /etc/ImageMagick-6/policy.xml
-sudo cp autre/EXAMPLE_policy.xml /etc/ImageMagick-6/policy.xml
 sudo apt install python3-opencv
 sudo apt-get install vlc
 sudo apt install ffmpeg
